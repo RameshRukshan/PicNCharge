7117bf1e23cb67bf1d121b49ca12707332b88241
Ramesh Rukshan <ramesharukshan3@gmail.com>
2023-11-11 02:07:10 +0530

    .test

---
+++ app/src/main/java/com/picncharge/androidapp/welcome.kt
@@ -0,0 +1,11 @@
+package com.picncharge.androidapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class welcome : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
