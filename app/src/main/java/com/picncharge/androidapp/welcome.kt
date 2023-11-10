7c4e3abf07340ead008c029ac56ee29d45eabeb0
Ramesh Rukshan <ramesharukshan3@gmail.com>
2023-11-11 02:30:29 +0530

    experiment one

--- app/src/main/java/com/picncharge/androidapp/welcome.kt
+++ app/src/main/java/com/picncharge/androidapp/welcome.kt
@@ -7,5 +7,7 @@ class welcome : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        w
     }
 }
