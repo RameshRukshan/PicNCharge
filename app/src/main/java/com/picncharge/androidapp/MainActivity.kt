b50ced9578773a1c2c0b175fc319aa0f7de84bb3
Ramesh Rukshan <ramesharukshan3@gmail.com>
2023-11-12 19:05:30 +0530

    redirect to logn

--- app/src/main/java/com/picncharge/androidapp/MainActivity.kt
+++ app/src/main/java/com/picncharge/androidapp/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.picncharge.androidapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btn_continue : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_continue = findViewById(R.id.btn_welcome_continue)
+
+        btn_continue.setOnClickListener(){
+            var go_to_login = Intent(this, login::class.java)
+            startActivity(go_to_login)
+        }
     }
 }
