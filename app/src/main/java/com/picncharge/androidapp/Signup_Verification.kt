c76ecc332c2e0cdb0e98aa449b723124d8fc2fc2
Thamalu Amarasingha <thamalu7501@gmail.com>
2023-11-12 20:37:57 +0530

    Signup verification activity created

---
+++ app/src/main/java/com/picncharge/androidapp/Signup_Verification.kt
@@ -0,0 +1,11 @@
+package com.picncharge.androidapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Signup_Verification : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup_verification)
+    }
+}
