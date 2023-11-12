41974d1fab17a881bf3c91b991aadcf83825c075
Kavinda Apuuhamy <145139167+KavindaAppuhamy@users.noreply.github.com>
2023-11-13 02:43:43 +0530

    Done the Verification UI

--- app/src/main/java/com/picncharge/androidapp/sign_up.kt
+++ app/src/main/java/com/picncharge/androidapp/sign_up.kt
@@ -1,11 +1,22 @@
 package com.picncharge.androidapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class sign_up : AppCompatActivity() {
+
+    lateinit var btn_createAccount : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        btn_createAccount = findViewById(R.id.btn_signup_createaccount)
+
+        btn_createAccount.setOnClickListener(){
+            var go_to_signup_verification = Intent(this, Signup_Verification::class.java)
+            startActivity(go_to_signup_verification)
+        }
     }
 }
