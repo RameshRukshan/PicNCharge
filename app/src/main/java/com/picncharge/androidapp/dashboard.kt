401885e9f41bed5cd4eed3c7a18c64f60fa9f2a4
Ramesh Rukshan <ramesharukshan3@gmail.com>
2023-11-13 10:43:43 +0530

    bottom Navigation created

--- app/src/main/java/com/picncharge/androidapp/dashboard.kt
+++ app/src/main/java/com/picncharge/androidapp/dashboard.kt
@@ -2,10 +2,22 @@ package com.picncharge.androidapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.navigation.NavigationView
+import com.picncharge.androidapp.databinding.ActivityMainBinding
 
-class dashboard : AppCompatActivity() {
+class dashboard : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var fragmentManager : FragmentManager
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_dashboard)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        TODO("Not yet implemented")
     }
 }
