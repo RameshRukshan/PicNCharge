37276292c148e97228f9e3d5013ec917a040966f
Ramesh Rukshan <ramesharukshan3@gmail.com>
2023-11-13 20:54:35 +0530

    Dashboard Nav bar UI Created

--- app/src/main/java/com/picncharge/androidapp/dashboard.kt
+++ app/src/main/java/com/picncharge/androidapp/dashboard.kt
@@ -2,17 +2,15 @@ package com.picncharge.androidapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
-import com.google.android.material.navigation.NavigationView
+import androidx.fragment.app.FragmentManager
+import com.picncharge.androidapp.databinding.ActivityMainBinding
 
-class dashboard : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+class dashboard : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
     }
 
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        TODO("Not yet implemented")
-    }
 }
