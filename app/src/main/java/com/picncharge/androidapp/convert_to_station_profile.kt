20d5cae96f3de94a7bd5b03212d1040de35973a2
Kavinda Apuuhamy <145139167+KavindaAppuhamy@users.noreply.github.com>
2023-11-14 20:21:53 +0530

    Convert To Station  Profile UI Created
    Ramesh i cant understand how to align these elements so please do that.

---
+++ app/src/main/java/com/picncharge/androidapp/convert_to_station_profile.kt
@@ -0,0 +1,11 @@
+package com.picncharge.androidapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class convert_to_station_profile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_convert_to_station_profile)
+    }
+}
