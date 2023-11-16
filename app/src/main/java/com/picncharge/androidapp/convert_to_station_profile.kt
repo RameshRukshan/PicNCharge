5387dc019ab51d86501bffb779340adf81c3346e
Kavinda Apuuhamy <145139167+KavindaAppuhamy@users.noreply.github.com>
2023-11-17 02:59:09 +0530

    I Made Verification  Station Profile Page UI
    Verification Station Profile UI Done
    and also I programed Action Button for Open this page in Convert to Station Profile Page.

--- app/src/main/java/com/picncharge/androidapp/convert_to_station_profile.kt
+++ app/src/main/java/com/picncharge/androidapp/convert_to_station_profile.kt
@@ -1,11 +1,22 @@
 package com.picncharge.androidapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class convert_to_station_profile : AppCompatActivity() {
+
+    lateinit var btn_continue : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_convert_to_station_profile)
+
+        btn_continue = findViewById(R.id.btn_convert_to_station_continue)
+
+        btn_continue.setOnClickListener(){
+            var go_to_verification_station_profile = Intent(this, Verification_Station_Profile::class.java)
+            startActivity(go_to_verification_station_profile)
+        }
     }
 }
