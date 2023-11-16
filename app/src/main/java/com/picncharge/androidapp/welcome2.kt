f77153776a85ef1ddd93515251ca47e9f2846299
Kavinda Apuuhamy <145139167+KavindaAppuhamy@users.noreply.github.com>
2023-11-16 19:40:23 +0530

    I completed the Welcome2 Page UI
    Completed Welcome2 UI and Welcome Button java Action code.

--- app/src/main/java/com/picncharge/androidapp/welcome2.kt
+++ app/src/main/java/com/picncharge/androidapp/welcome2.kt
@@ -1,11 +1,22 @@
 package com.picncharge.androidapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class welcome2 : AppCompatActivity() {
+
+    lateinit var btn_welcome_next : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome2)
+
+        btn_welcome_next = findViewById(R.id.btn_welcome_next)
+
+        btn_welcome_next.setOnClickListener(){
+            var go_to_convert_to_station_profile = Intent(this, convert_to_station_profile::class.java)
+            startActivity(go_to_convert_to_station_profile)
+        }
     }
 }
