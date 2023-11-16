8bc56fa389c5499f23044c1be0ce354e10d2ab83
Ramesh Rukshan <ramesharukshan3@gmail.com>
2023-11-16 13:18:49 +0530

    Change Password UI added

---
+++ app/src/main/java/com/picncharge/androidapp/change_password.kt
@@ -0,0 +1,11 @@
+package com.picncharge.androidapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class change_password : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change_password)
+    }
+}
