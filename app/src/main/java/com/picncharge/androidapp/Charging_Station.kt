24562b525470f616fd863822048b9504e403e7fe
Ramesh Rukshan <ramesharukshan3@gmail.com>
2023-11-24 20:41:45 +0530

    Charging Station UI Created

---
+++ app/src/main/java/com/picncharge/androidapp/Charging_Station.kt
@@ -0,0 +1,11 @@
+package com.picncharge.androidapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Charging_Station : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_charging_station)
+    }
+}
