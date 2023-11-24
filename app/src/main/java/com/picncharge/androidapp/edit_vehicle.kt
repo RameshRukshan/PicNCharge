dc550ea6257123bd24adc4fb2132bf2b394ff149
Ramesh Rukshan <ramesharukshan3@gmail.com>
2023-11-24 18:31:44 +0530

    Edit Vehicle UI Done

---
+++ app/src/main/java/com/picncharge/androidapp/edit_vehicle.kt
@@ -0,0 +1,11 @@
+package com.picncharge.androidapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class edit_vehicle : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_vehicle)
+    }
+}
