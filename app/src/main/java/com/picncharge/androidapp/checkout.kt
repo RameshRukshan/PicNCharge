a84238f715516814fe91deee781fa6e978cf68ad
Ramesh Rukshan <ramesharukshan3@gmail.com>
2023-11-24 23:38:29 +0530

    .

---
+++ app/src/main/java/com/picncharge/androidapp/checkout.kt
@@ -0,0 +1,11 @@
+package com.picncharge.androidapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class checkout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_checkout)
+    }
+}
