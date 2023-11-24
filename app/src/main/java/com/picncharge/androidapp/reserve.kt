e56b9c7744d6c42987c140e62fc04fb7adb8eaf1
Ramesh Rukshan <ramesharukshan3@gmail.com>
2023-11-24 22:23:14 +0530

    Reservation UI added

---
+++ app/src/main/java/com/picncharge/androidapp/reserve.kt
@@ -0,0 +1,11 @@
+package com.picncharge.androidapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class reserve : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reserve)
+    }
+}
