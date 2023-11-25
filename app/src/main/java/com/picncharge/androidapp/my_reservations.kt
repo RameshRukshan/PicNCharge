07057f7b79b933bf5b4c2b62e8a1e9648eab27ff
Ramesh Rukshan <ramesharukshan3@gmail.com>
2023-11-25 15:02:43 +0530

    My Reservations UI Done

---
+++ app/src/main/java/com/picncharge/androidapp/my_reservations.kt
@@ -0,0 +1,11 @@
+package com.picncharge.androidapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class my_reservations : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_reservations)
+    }
+}
