2c5725c1827d8ab70d65f9a37789c27cfded1fc1
Ramesh Rukshan <ramesharukshan3@gmail.com>
2023-11-25 17:06:41 +0530

    View reservation Nav Menu function

--- app/src/main/java/com/picncharge/androidapp/view_reservation.kt
+++ app/src/main/java/com/picncharge/androidapp/view_reservation.kt
@@ -1,11 +1,44 @@
 package com.picncharge.androidapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class view_reservation : AppCompatActivity() {
+
+    lateinit var nav_home : Button
+    lateinit var nav_map : Button
+    lateinit var nav_profile : Button
+    lateinit var nav_notification : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_reservation)
+
+        nav_home = findViewById(R.id.btn_nav_home)
+        nav_map = findViewById(R.id.btn_nav_map)
+        nav_profile = findViewById(R.id.btn_nav_profile)
+        nav_notification = findViewById(R.id.btn_nav_notification)
+
+        nav_home.setOnClickListener(){
+            var go_to_home = Intent(this, dashboard::class.java)
+            startActivity(go_to_home)
+        }
+
+        nav_map.setOnClickListener(){
+            var go_to_map = Intent(this, map::class.java)
+            startActivity(go_to_map)
+        }
+
+        nav_profile.setOnClickListener(){
+            var go_to_profile = Intent(this, profile::class.java)
+            startActivity(go_to_profile)
+        }
+
+        nav_notification.setOnClickListener(){
+            var go_to_notification = Intent(this, notification::class.java)
+            startActivity(go_to_notification)
+        }
     }
 }
