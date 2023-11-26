6c0280bb7aa5319b71d6e254e0d6d4fcb0ee6a2d
Ramesh Rukshan <ramesharukshan3@gmail.com>
2023-11-26 19:26:11 +0530

    verification redirection done

--- app/src/main/java/com/picncharge/androidapp/Signup_Verification.kt
+++ app/src/main/java/com/picncharge/androidapp/Signup_Verification.kt
@@ -6,8 +6,16 @@ import android.os.Bundle
 import android.widget.Button
 
 class Signup_Verification : AppCompatActivity() {
+    lateinit var btn_verify : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup_verification)
+
+        btn_verify = findViewById(R.id.btn_verification_Verify)
+
+        btn_verify.setOnClickListener(){
+            var go_to_home = Intent(this, dashboard::class.java)
+            startActivity(go_to_home)
+        }
     }
 }
